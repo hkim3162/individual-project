d236803d8fe5e7ad2b66954fc2d879e0bee2ba47
hkim3162 <hkim3162@gmail.com>
2018-10-31 13:40:53 -0500

    Communicating between front and back end, with react and rails. Renders back end data

---
+++ website/db/migrate/20181031031138_create_photos.rb
@@ -0,0 +1,14 @@
+class CreatePhotos < ActiveRecord::Migration[5.2]
+  def change
+    create_table :photos do |t|
+      t.string :url
+      t.string :description
+      t.string :category
+      t.string :client
+      t.date :date
+  
+
+      t.timestamps
+    end
+  end
+end
